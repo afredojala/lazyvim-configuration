083daa05676526a36a03b96452396f97ebe9caf7
Andreas F-O <andreasfo@gmail.com>
2023-01-30 22:24:06 +0100

    Added python host as env var

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.python3_host_prog = "/home/andreasfo/.config/nvim_python/.venv/bin/python"
+local python3_host = os.getenv("NVIM_PYTHON")
+vim.g.python3_host_prog = python3_host
